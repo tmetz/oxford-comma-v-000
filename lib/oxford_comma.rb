5ab685977686a42a2c26b0deef0fe460e0bc7fca
Tammy Metz <tammy@tammymetz.com>
2018-07-15 05:11:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    new_last_word = "and #{array[-1]}"
+    array[-1] = new_last_word
+    array.join(", ")
+  end
 end
